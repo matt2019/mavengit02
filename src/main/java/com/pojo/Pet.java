162f0121cec0d4130c6b13f6a0a89eedfc2c2c5c
matt2019 <40055476+matt2019@users.noreply.github.com>
2018-08-07 15:54:31 +0800

    Update Pet.java
    描述不可用

--- src/main/java/com/pojo/Pet.java
+++ src/main/java/com/pojo/Pet.java
@@ -3,6 +3,7 @@ package com.pojo;
 public class Pet {
     public void show(){
         System.out.println("i am e pet");
-        System.out.println("12312");
+        System.out.println("12312");//新增的这个代码不符合要求，请重新修改
+    
     }
 }
